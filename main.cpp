dcbbc3e7765390f7efe764f5c66ae9231be7aab8
Christian Jaeger <ch@christianjaeger.ch>
2021-12-19 18:43:04 +0000

    Translated example

--- main.cpp
+++ main.cpp
@@ -8,21 +8,50 @@
 
 #include <vector>
 
+template<typename T>
+T square(T x) {
+    return x * x;
+}
 
-int foo(int n) {
-    std::vector<float> v;
-    v.resize(n);
-    for (int i = 0; i < n; i++) {
-        v[i] = 12.345;
+typedef std::vector<double> vec;
+
+void zero(vec &v) {
+    auto n = v.size();
+    for (size_t i = 0; i < n; i++) {
+        v[i] = 0;
     }
-    return 0;
+}
+
+void pot2(vec &a) {
+    auto n = a.size();
+    vec b;
+    b.resize(n);
+    zero(b);
+    for (size_t i = 0; i < n; i++) {
+        b[i] = square(a[i]);
+    }
+}
+
+void pot(vec &a, size_t index) {
+    a[index] = square(a[index]);
+}
+
+void bla(size_t n) {
+    vec a;
+    a.resize(n);
+    auto b = a;
+    for (size_t i = 0; i < n; i++) {
+        pot(a, i);
+    }
+
+    pot2(b);
 }
 
 
 int main() {
-    int n;
+    size_t n;
     std::cin >> n;
     std::cout << "n = " << n << std::endl;
-    foo(n);
+    bla(n);
     return 0;
 }
